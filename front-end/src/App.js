c218c4486fde6b44e17316e0b0eb7c3677627df9
Xico <francisco100eg@gmail.com>
2023-03-28 17:22:28 -0300

    Feat: Redirect home  to login route front-end and created migration and seeders backend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Login from './Pages/Login';
 import rockGlass from './images/rockGlass.svg';
 
-//Start
-
 function App() {
+
   return (
     <div className="App">
       <span className="logo">TRYBE</span>
       <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
         Glass
       </object>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
