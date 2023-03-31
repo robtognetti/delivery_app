e521b7f501c7cbf58ac709a4c45d646c46349724
Xico <francisco100eg@gmail.com>
2023-03-31 16:54:32 -0300

    Feat: Added seller orders

--- back-end/src/api/routes/salesRoute.js
+++ back-end/src/api/routes/salesRoute.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const { salesCreateController, salesGetController } = require('../controllers/SaleController');
+const {
+  salesCreateController,
+  salesGetController,
+  salesGetSellerController,
+} = require('../controllers/SaleController');
 
 const salesRoute = express.Router();
 
-salesRoute.post(
-  '/sales',
-  salesCreateController,
-);
+salesRoute.post('/sales', salesCreateController);
 
 salesRoute.post('/salesGet', (req, res) => salesGetController(req, res));
+salesRoute.post('/salesGetSeller', (req, res) =>
+  salesGetSellerController(req, res));
 module.exports = salesRoute;
